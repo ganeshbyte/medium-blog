a7b5c376d1ee1eedee674666553b010f1ee85406
ganeshbyte <gc930954@gmail.com>
2024-11-28 08:35:41 +0530

    support for common zod types

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,33 +1,18 @@
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 import { Hono } from "hono";
+import { decode, sign, verify } from "hono/jwt";
+import { blogRouter } from "../routes/blog";
+import { userRouter } from "../routes/user";
 
-const app = new Hono();
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  };
+}>();
 
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
-});
-
-app.post("/api/v1/signin", (c) => {
-  return c.text("singin");
-});
-app.post("/api/v1/signup", (c) => {
-  return c.text("signup");
-});
-
-//blog routes
-app.get("/api/v1/blog", (c) => {
-  return c.text("blogs");
-});
-
-app.get("/api/v1/blog/:id", (c) => {
-  return c.text("blogs");
-});
-
-app.put("/api/v1/blog", (c) => {
-  return c.text("Update Blog");
-});
-
-app.post("/api/v1/blog", (c) => {
-  return c.text("Create Blog");
-});
+app.route("/api/v1/blog", blogRouter);
+app.route("/api/v1/user", userRouter);
 
 export default app;
